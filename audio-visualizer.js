bd5e74137e2113eb6e2e7a89191ca65cf06c97c3
Ryan Fong <ryanfong@Ryans-MacBook-Pro.local>
2017-04-09 16:38:11 -0700

    Add two sources to the analyser

--- audio-visualizer.js
+++ audio-visualizer.js
@@ -1,4 +1,33 @@
+const sounds = {
+  90: './sounds/Kick-808-Tone6-q.wav', // z
+  88: './sounds/Kick-909-Tune10-i.wav', // x
+  67: './sounds/Kick-90s-3.wav', // c
+  86: './sounds/Kick-90s-2.wav', // v
+  66: './sounds/Kick-Sub-909-Long.wav', // b
+  78: './sounds/Kick-808-Thud.wav', // n
+  77: './sounds/Kick-Big-Sine.wav', // m
 
+  65: './sounds/Tom-505-ASR-1.wav', // a
+  83: './sounds/Tom-505-ASR-2.wav', // s
+  68: './sounds/Tom-505-ASR-3.wav', // d
+  70: './sounds/Snare-BamVin.wav', // f
+  71: './sounds/Snare-70s-MPC-1.wav', // g
+  72: './sounds/Snare-Vinyl-01.wav', // h
+  74: './sounds/Tom-DDD-Fatx-1.wav', // j
+  75: './sounds/Tom-DDD-Fatx-2.wav', // k
+  76: './sounds/Tom-DDD-Fatx-3.wav', // l
+
+  81: './sounds/Hihat-505-ASR-Open.wav', //q
+  87: './sounds/hey.wav', //w
+  69: './sounds/Perc-Bolly-1.wav', //e
+  82: './sounds/Tamb-ShortAndSolid.wav', //r
+  84: './sounds/Cymbal-Boring.wav', //t
+  89: './sounds/Cowbell-SP.wav', //y
+  85: './sounds/Clap_BitNighties.wav', //u
+  73: './sounds/Hihat-80sSpaceCraft-Closed.wav', //i
+  79: './sounds/Ride-Real-SP-2.wav', //o
+  80: './sounds/Cabassa SP.wav', //p
+};
 
 $(document).ready( function () {
     // let audioElement = document.getElementById('audioElement');
@@ -20,8 +49,8 @@ $(document).ready( function () {
     //     $("#currentTime").text("Current second:" + audioElement.currentTime);
     // });
 
-    let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    let analyser = audioCtx.createAnalyser();
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    const analyser = audioCtx.createAnalyser();
     let source;
     let source2;
 
@@ -32,11 +61,11 @@ $(document).ready( function () {
     // });
 
 // let audioSrc = audioCtx.createMediaElementSource(audioElement);
-function getData() {
+function getData1(sound) {
   source = audioCtx.createBufferSource();
   let request = new XMLHttpRequest();
 
-  request.open('GET', './audio/turn.mp3', true);
+  request.open('GET', `./audio/${sound}.mp3`, true);
 
   request.responseType = 'arraybuffer';
 
@@ -50,6 +79,32 @@ function getData() {
         source.connect(analyser);
         source.connect(audioCtx.destination);
         source.loop = true;
+      },
+
+      function(e){ console.log("Error with decoding audio data" + e.err); });
+
+  };
+
+  request.send();
+}
+function getData2(sound) {
+  source2 = audioCtx.createBufferSource();
+  let request = new XMLHttpRequest();
+
+  request.open('GET', `${sound}`, true);
+
+  request.responseType = 'arraybuffer';
+
+
+  request.onload = function() {
+    let audioData = request.response;
+
+    audioCtx.decodeAudioData(audioData, function(buffer) {
+        source2.buffer = buffer;
+
+        source2.connect(analyser);
+        source2.connect(audioCtx.destination);
+        source2.loop = false;
       },
 
       function(e){ console.log("Error with decoding audio data" + e.err); });
@@ -142,8 +197,20 @@ let frequencyData = new Uint8Array(200);
       });
   }
 
+  document.onkeydown = function(e) {
+    let soundId = sounds[e.keyCode];
+    let sound;
+    if (soundId) {
+      sound = new Howl({
+        src: [soundId]
+      });
+    }
+    getData2(soundId);
+    source2.start(0);
+  };
+
   $('#play').click(function() {
-    getData();
+    getData1('slow');
     source.start(0);
       $("#status").text("Status: Playing");
   });
--- sounds.js
+++ sounds.js
@@ -1,44 +1,45 @@
-const sounds = {
-  90: './sounds/Kick-808-Tone6-q.wav', // z
-  88: './sounds/Kick-909-Tune10-i.wav', // x
-  67: './sounds/Kick-90s-3.wav', // c
-  86: './sounds/Kick-90s-2.wav', // v
-  66: './sounds/Kick-Sub-909-Long.wav', // b
-  78: './sounds/Kick-808-Thud.wav', // n
-  77: './sounds/Kick-Big-Sine.wav', // m
+// module.exports = {
+//   90: './sounds/Kick-808-Tone6-q.wav', // z
+//   88: './sounds/Kick-909-Tune10-i.wav', // x
+//   67: './sounds/Kick-90s-3.wav', // c
+//   86: './sounds/Kick-90s-2.wav', // v
+//   66: './sounds/Kick-Sub-909-Long.wav', // b
+//   78: './sounds/Kick-808-Thud.wav', // n
+//   77: './sounds/Kick-Big-Sine.wav', // m
+//
+//   65: './sounds/Tom-505-ASR-1.wav', // a
+//   83: './sounds/Tom-505-ASR-2.wav', // s
+//   68: './sounds/Tom-505-ASR-3.wav', // d
+//   70: './sounds/Snare-BamVin.wav', // f
+//   71: './sounds/Snare-70s-MPC-1.wav', // g
+//   72: './sounds/Snare-Vinyl-01.wav', // h
+//   74: './sounds/Tom-DDD-Fatx-1.wav', // j
+//   75: './sounds/Tom-DDD-Fatx-2.wav', // k
+//   76: './sounds/Tom-DDD-Fatx-3.wav', // l
+//
+//   81: './sounds/Hihat-505-ASR-Open.wav', //q
+//   87: './sounds/hey.wav', //w
+//   69: './sounds/Perc-Bolly-1.wav', //e
+//   82: './sounds/Tamb-ShortAndSolid.wav', //r
+//   84: './sounds/Cymbal-Boring.wav', //t
+//   89: './sounds/Cowbell-SP.wav', //y
+//   85: './sounds/Clap_BitNighties.wav', //u
+//   73: './sounds/Hihat-80sSpaceCraft-Closed.wav', //i
+//   79: './sounds/Ride-Real-SP-2.wav', //o
+//   80: './sounds/Cabassa SP.wav', //p
+// };
 
-  65: './sounds/Tom-505-ASR-1.wav', // a
-  83: './sounds/Tom-505-ASR-2.wav', // s
-  68: './sounds/Tom-505-ASR-3.wav', // d
-  70: './sounds/Snare-BamVin.wav', // f
-  71: './sounds/Snare-70s-MPC-1.wav', // g
-  72: './sounds/Snare-Vinyl-01.wav', // h
-  74: './sounds/Tom-DDD-Fatx-1.wav', // j
-  75: './sounds/Tom-DDD-Fatx-2.wav', // k
-  76: './sounds/Tom-DDD-Fatx-3.wav', // l
 
-  81: './sounds/Hihat-505-ASR-Open.wav', //q
-  87: './sounds/hey.wav', //w
-  69: './sounds/Perc-Bolly-1.wav', //e
-  82: './sounds/Tamb-ShortAndSolid.wav', //r
-  84: './sounds/Cymbal-Boring.wav', //t
-  89: './sounds/Cowbell-SP.wav', //y
-  85: './sounds/Clap_BitNighties.wav', //u
-  73: './sounds/Hihat-80sSpaceCraft-Closed.wav', //i
-  79: './sounds/Ride-Real-SP-2.wav', //o
-  80: './sounds/Cabassa SP.wav', //p
-};
-
-document.onkeydown = function(e) {
-  let soundId = sounds[e.keyCode];
-  let sound;
-  if (soundId) {
-    sound = new Howl({
-      src: [soundId]
-    });
-  }
-  sound.play();
-};
+// document.onkeydown = function(e) {
+//   let soundId = sounds[e.keyCode];
+//   let sound;
+//   if (soundId) {
+//     sound = new Howl({
+//       src: [soundId]
+//     });
+//   }
+//   sound.play();
+// };
 
 //   90: 'bass1', // z
 //   88: 'bass2', // x
